74dcf39a0f588dd1da60f6c753b594de668375d3
tim6248 <tim6248@gmail.com>
2018-07-01 17:20:35 +0900

    Add Delete Action at Contact Activity

--- app/src/main/java/com/example/q/cs496_app1/ContactActivity.java
+++ app/src/main/java/com/example/q/cs496_app1/ContactActivity.java
@@ -9,6 +9,19 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+
 public class ContactActivity extends Activity {
 
     public static Context CONTACT_CONTEXT;
@@ -16,6 +29,9 @@ public class ContactActivity extends Activity {
     TextView tvName;
     TextView tvPhoneNumber;
     Button editButton;
+    Button deleteButton;
+
+    TextView result;
 
     int itemPosition;
 
@@ -34,6 +50,8 @@ public class ContactActivity extends Activity {
         tvName.setText(name);
         tvPhoneNumber.setText(phoneNumber);
 
+        result = (TextView)findViewById(R.id.result);
+
         editButton = (Button)findViewById(R.id.edit_button);
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +61,42 @@ public class ContactActivity extends Activity {
                 intent.putExtra("name", tvName.getText());
                 intent.putExtra("phoneNumber", tvPhoneNumber.getText());
                 startActivity(intent);
+            }
+        });
+
+        deleteButton = (Button)findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Gson gson = new Gson();
+
+                try {
+                    StringBuffer data = new StringBuffer();
+                    FileInputStream org = openFileInput("test.json");
+                    BufferedReader br = new BufferedReader(new InputStreamReader(org));
+                    String str = br.readLine();
+                    while (str != null) {
+                        data.append(str + "\n");
+                        str = br.readLine();
+                    }
+
+                    List<ContactItem> orgList =  gson.fromJson(data.toString(),
+                            new TypeToken<List<ContactItem>>(){}.getType());
+                    Collections.sort(orgList, new ContactSorting());
+
+                    orgList.remove(itemPosition);
+
+                    String json = gson.toJson(orgList);
+                    result.setText(json);
+
+                    FileOutputStream fos = openFileOutput("test.json", Context.MODE_PRIVATE);
+                    fos.write(json.getBytes());
+                    fos.close();
+                    Toast.makeText(ContactActivity.this, "Delete Success", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(ContactActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                onStop();
             }
         });
     }
