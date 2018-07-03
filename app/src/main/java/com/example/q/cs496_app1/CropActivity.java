59c80514462bc5f9903ccf36a4548fb3aac3de5d
tim6248 <tim6248@gmail.com>
2018-07-03 13:53:11 +0900

    Try Crop activity but not implemented

---
+++ app/src/main/java/com/example/q/cs496_app1/CropActivity.java
@@ -0,0 +1,129 @@
+package com.example.q.cs496_app1;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class CropActivitiy extends Activity implements View.OnClickListener {
+
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int CROP_FROM_CAMERA = 2;
+
+    private Uri mImageCaptureUri;
+    private ImageView mPhotoImageView;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop);
+
+        mButton = (Button) findViewById(R.id.button);
+        mPhotoImageView = (ImageView) findViewById(R.id.imageView);
+
+        mButton.setOnClickListener(this);
+    }
+
+    private void doTakePhotoAction() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, PICK_FROM_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        switch (requestCode) {
+            case CROP_FROM_CAMERA:
+            {
+                final Bundle extras = data.getExtras();
+
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    mPhotoImageView.setImageBitmap(photo);
+                }
+
+                File f = new File(mImageCaptureUri.getPath());
+                if (f.exists()) {
+                    f. delete();
+                }
+
+                break;
+            }
+
+            case PICK_FROM_ALBUM:
+            {
+                mImageCaptureUri = data.getData();
+            }
+
+            case PICK_FROM_CAMERA:
+            {
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageCaptureUri, "image/*");
+
+                intent.putExtra("outputX", 90);
+                intent.putExtra("outputY", 90);
+                intent.putExtra("aspectX", 1);
+                intent.putExtra("aspectY", 1);
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_CAMERA);
+
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                doTakePhotoAction();
+            }
+        };
+
+        DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        };
+
+        DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        };
+
+        new AlertDialog.Builder(this)
+                .setTitle("업로드 할 이미지 선택")
+                .setPositiveButton("Camera", cameraListener)
+                .setNeutralButton("Album", albumListener)
+                .setNegativeButton("Cancel", cancelListener)
+                .create().show();
+    }
+}
