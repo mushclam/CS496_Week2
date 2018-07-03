b172ff017693b0b8f445bdff363e2629e9f55d2c
tim6248 <tim6248@gmail.com>
2018-07-03 19:45:54 +0900

    Add 2048 in Third Fragment

---
+++ app/src/main/java/com/example/q/cs496_app1/tabs/third/ThirdFragment.java
@@ -0,0 +1,234 @@
+package com.example.q.cs496_app1.tabs.third;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.q.cs496_app1.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ThirdFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ThirdFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ThirdFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private Context mContext;
+
+    private static final String WIDTH = "width";
+    private static final String HEIGHT = "height";
+    private static final String SCORE = "score";
+    private static final String HIGH_SCORE = "high score temp";
+    private static final String UNDO_SCORE = "undo score";
+    private static final String CAN_UNDO = "can undo";
+    private static final String UNDO_GRID = "undo";
+    private static final String GAME_STATE = "game state";
+    private static final String UNDO_GAME_STATE = "undo game state";
+    public MainView view;
+
+//    private OnFragmentInteractionListener mListener;
+
+    public ThirdFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ThirdFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ThirdFragment newInstance(String param1, String param2) {
+        ThirdFragment fragment = new ThirdFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mContext = getActivity();
+        view = new MainView(mContext);
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(mContext);
+        view.hasSaveState = settings.getBoolean("save_state", false);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.getBoolean("hasState")) {
+                load();
+            }
+        }
+        return view;
+    }
+
+//    public boolean onFragmentKeyDown(int keyCode, KeyEvent event) {
+//        if (keyCode == KeyEvent.KEYCODE_MENU) {
+//            //Do nothing
+//            return true;
+//        } else if (keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
+//            view.game.move(2);
+//            return true;
+//        } else if (keyCode == KeyEvent.KEYCODE_DPAD_UP) {
+//            view.game.move(0);
+//            return true;
+//        } else if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
+//            view.game.move(3);
+//            return true;
+//        } else if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) {
+//            view.game.move(1);
+//            return true;
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean("hasState", true);
+        save();
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void onPause() {
+        super.onPause();
+        save();
+    }
+
+    private void save() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(mContext);
+        SharedPreferences.Editor editor = settings.edit();
+        Tile[][] field = view.game.grid.field;
+        Tile[][] undoField = view.game.grid.undoField;
+        editor.putInt(WIDTH, field.length);
+        editor.putInt(HEIGHT, field.length);
+        for (int xx = 0; xx < field.length; xx++) {
+            for (int yy = 0; yy < field[0].length; yy++) {
+                if (field[xx][yy] != null) {
+                    editor.putInt(xx + " " + yy, field[xx][yy].getValue());
+                } else {
+                    editor.putInt(xx + " " + yy, 0);
+                }
+
+                if (undoField[xx][yy] != null) {
+                    editor.putInt(UNDO_GRID + xx + " " + yy, undoField[xx][yy].getValue());
+                } else {
+                    editor.putInt(UNDO_GRID + xx + " " + yy, 0);
+                }
+            }
+        }
+        editor.putLong(SCORE, view.game.score);
+        editor.putLong(HIGH_SCORE, view.game.highScore);
+        editor.putLong(UNDO_SCORE, view.game.lastScore);
+        editor.putBoolean(CAN_UNDO, view.game.canUndo);
+        editor.putInt(GAME_STATE, view.game.gameState);
+        editor.putInt(UNDO_GAME_STATE, view.game.lastGameState);
+        editor.commit();
+    }
+
+    public void onResume() {
+        super.onResume();
+        load();
+    }
+
+    private void load() {
+        //Stopping all animations
+        view.game.aGrid.cancelAnimations();
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(mContext);
+        for (int xx = 0; xx < view.game.grid.field.length; xx++) {
+            for (int yy = 0; yy < view.game.grid.field[0].length; yy++) {
+                int value = settings.getInt(xx + " " + yy, -1);
+                if (value > 0) {
+                    view.game.grid.field[xx][yy] = new Tile(xx, yy, value);
+                } else if (value == 0) {
+                    view.game.grid.field[xx][yy] = null;
+                }
+
+                int undoValue = settings.getInt(UNDO_GRID + xx + " " + yy, -1);
+                if (undoValue > 0) {
+                    view.game.grid.undoField[xx][yy] = new Tile(xx, yy, undoValue);
+                } else if (value == 0) {
+                    view.game.grid.undoField[xx][yy] = null;
+                }
+            }
+        }
+
+        view.game.score = settings.getLong(SCORE, view.game.score);
+        view.game.highScore = settings.getLong(HIGH_SCORE, view.game.highScore);
+        view.game.lastScore = settings.getLong(UNDO_SCORE, view.game.lastScore);
+        view.game.canUndo = settings.getBoolean(CAN_UNDO, view.game.canUndo);
+        view.game.gameState = settings.getInt(GAME_STATE, view.game.gameState);
+        view.game.lastGameState = settings.getInt(UNDO_GAME_STATE, view.game.lastGameState);
+    }
+
+//    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+//
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
+}
