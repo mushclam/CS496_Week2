f1bfd4dbd06304d0e14062b43f80b24e1b76afc1
tim6248 <tim6248@gmail.com>
2018-07-05 18:53:04 +0900

    change splash

--- app/src/main/java/com/example/q/cs496_app1/SplashActivity.java
+++ app/src/main/java/com/example/q/cs496_app1/SplashActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         final Intent intent = new Intent(this, MainActivity.class);
 
